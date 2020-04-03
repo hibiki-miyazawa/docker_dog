ea6dbfe6c4d7c44937c28099f6185e5c7975809a
hibi.kicks <hibikimiyazawa@gmail.com>
2020-04-03 22:15:46 +0900

    Set static-pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def full_title(page_title = '')
+        base_title = "Community for Dogs"
+        if page_title.empty?
+            base_title
+        else
+            page_title + "|" + base_title
+        end
+    end
 end
