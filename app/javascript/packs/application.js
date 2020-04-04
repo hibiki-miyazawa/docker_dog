fb51731b3e3c16131eac4bc2a386d2707f54bb72
hibi.kicks <hibikimiyazawa@gmail.com>
2020-04-04 12:34:19 +0900

    Change app/javascript/packs/application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,14 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import '../javascripts/application'
+import '../stylesheets/application'
+
+const images = require.context('../images', true)
+
+require("trix")
+require("@rails/actiontext")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
