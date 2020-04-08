efac530e9012e046cd968e0cba9b837a5eada067
hibi.kicks <hibikimiyazawa@gmail.com>
2020-04-08 10:08:06 +0900

    Add sessions con test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,4 +6,9 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get login" do
+    get login_path
+    assert_response :success
+  end
+
 end
