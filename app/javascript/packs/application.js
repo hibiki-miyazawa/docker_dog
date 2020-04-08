e0200c7697fa4563b11794405a6bc22675b0576b
hibi.kicks <hibikimiyazawa@gmail.com>
2020-04-08 16:15:16 +0900

    Install @rails/ujs & jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
+import Rails from '@rails/ujs'
+Rails.start()
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
