92405bdf3896328847fece865823182948cc24a1
hibi.kicks <hibikimiyazawa@gmail.com>
2020-04-10 11:30:37 +0900

    Change order application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,11 @@
 import 'bootstrap-material-design'
 import '../stylesheets/application'
 
+require("jquery")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
 
 import Rails from '@rails/ujs'
 Rails.start()
