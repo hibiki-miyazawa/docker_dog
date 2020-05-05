7cd05d1e033a409e1291fafb9637c30e842dde2a
hibi.kicks <hibikimiyazawa@gmail.com>
2020-05-05 12:34:16 +0900

    Remove index to dogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_030715) do
+ActiveRecord::Schema.define(version: 2020_05_04_155031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2020_05_05_030715) do
     t.string "image_name"
     t.string "color"
     t.string "gender"
-    t.index ["user_id", "created_at"], name: "index_dogs_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
