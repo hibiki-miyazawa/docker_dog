a255deac950468525d77dbbece6fa13a92faba1c
hibi.kicks <hibikimiyazawa@gmail.com>
2020-05-15 11:10:55 +0900

    Setting S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,16 +5,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production? 
-    CarrierWave.configure do |config|
-      config.fog_provider = 'fog/aws'
-      config.fog_credentials = {
-        :provider => 'AWS',
-        :region => ENV['S3_REGION'],
-        :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY']
-      }
-      config.fog_directory = ENV['S3_BUCKET']
-    end 
     storage :fog
   else
     storage :file
