8ad357a9084b31acf34f3d574d11a83dea09582f
hibi.kicks <hibikimiyazawa@gmail.com>
2020-05-18 17:05:52 +0900

    Add line 24

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   # RAILS_SERVE_STATIC_FILES = true
+  config.serve_static_assets = true
   config.public_file_server.enabled = true
   # ENV['RAILS_SERVE_STATIC_FILES'].present?
 
