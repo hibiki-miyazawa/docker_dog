c8c6f2d7a57df44bcb73fc3f77d392da056bea31
hibi.kicks <hibikimiyazawa@gmail.com>
2020-05-18 11:49:58 +0900

    Rewright line 23-25

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  #'RAILS_SERVE_STATIC_FILES' = true
-  config.server_static_files = true #ENV['RAILS_SERVE_STATIC_FILES'].present?
+  'RAILS_SERVE_STATIC_FILES' = true
+  config.server_static_files = true 
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
