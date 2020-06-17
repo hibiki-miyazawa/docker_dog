76683399e7f44289c16441a7b4cfb63a1bf6f9ad
hibi.kicks <hibikimiyazawa@gmail.com>
2020-06-18 08:14:32 +0900

    Add CSS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -94,6 +94,7 @@ class UsersController < ApplicationController
   def search
     if params[:prefecture_id].present?
       @users = User.where(prefecture_id: params[:prefecture_id])
+      @prefecture = Prefecture.find_by(id: params[:prefecture_id])
     else
       @users = User.none
     end
